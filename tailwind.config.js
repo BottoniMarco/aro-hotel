e2b1ab24e092536474896eb2543d82b81f98d659
marco bottoni <marco@alliza.ai>
2022-08-28 22:55:24 +0200

    aro-hotel

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'aro_green': '#12686E'
+      }
+    },
+  },
+  plugins: [],
+}
